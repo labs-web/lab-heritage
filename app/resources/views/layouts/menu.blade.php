267fd506c352eb3367b93d73412733195bc3df40
Boukhar <boukhar@kali.boukhar>
2024-02-17 16:46:10 -0500

    update front-end

--- app/resources/views/layouts/menu.blade.php
+++ app/resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <li class="nav-item">
     <a href="{{ route('membre.index') }}" class="nav-link {{ Request::is('membre.index') ? 'active' : '' }}">
-      <i class="nav-icon fas fa-table"></i>
+      <i class="nav-icon fas fa-users"></i>
       <p>Membres</p>
     </a>
 </li>
